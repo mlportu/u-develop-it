f471ad7c36ac894917e1d8d2d3fe895117451bf8
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-23 11:16:40 -0500

    api routes to select all candidates, single candidate and delete candiate

--- server.js
+++ server.js
@@ -16,6 +16,69 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database');
 });
 
+//Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err){
+        res.status(500).json({error: err.message})
+        return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//GET a single candidate
+app.get('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                WHERE id=?`;
+    const params = [req.params.id];   
+    db.get(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//Delete a candidate
+app.delete('/api/candidates/:id', (req, res) => {
+    const sql = `DELETE FROM candidates 
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if(err){
+            res.status(400).json({error: res.message});
+            return
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+// //Create a candidate
+// const sql =`INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+//     //ES5 function, not arrow functio, to use this 
+// db.run(sql, params, function(err, results){
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(results, this.lastID);
+// });
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
